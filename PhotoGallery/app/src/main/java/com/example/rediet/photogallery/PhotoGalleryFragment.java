6c9860884fc39f7a0635b0663fa43644e3e20e73
Rediet Tilahun Desta <redtil10@gmail.com>
2016-04-23 12:06:45 +0300

    finished chapter 27.

--- PhotoGallery/app/src/main/java/com/example/rediet/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/rediet/photogallery/PhotoGalleryFragment.java
@@ -1,7 +1,10 @@
 package com.example.rediet.photogallery;
 
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,8 +22,10 @@ import java.util.ArrayList;
  */
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
+
     GridView mGridView;
     ArrayList<GalleryItem> mItems;
+    ThumbnailDownloader<ImageView> mThumbnailThread;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,19 @@ public class PhotoGalleryFragment extends Fragment {
 
         setRetainInstance(true);
         new FetchItemsTask().execute();
-        Log.i(TAG, "I am in PHotoGallery");
+
+        mThumbnailThread = new ThumbnailDownloader<ImageView>(new Handler());
+        mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>(){
+            @Override
+            public void onThumbnailDownloaded(ImageView imageView, Bitmap thumbnail){
+                if(isVisible()){
+                    imageView.setImageBitmap(thumbnail);
+                }
+            }
+        });
+        mThumbnailThread.start();
+        mThumbnailThread.getLooper();
+        Log.i(TAG, "I am in PhotoGallery");
     }
 
     @Override
@@ -40,10 +58,23 @@ public class PhotoGalleryFragment extends Fragment {
         return v;
     }
 
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        mThumbnailThread.clearQueue();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mThumbnailThread.quit();
+        Log.i(TAG,"Background thread destroyed");
+    }
+
     void setupAdapter(){
         if(getActivity() == null || mGridView == null) return;
         if(mItems != null) {
-            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),android.R.layout.simple_gallery_item,mItems));
+            mGridView.setAdapter(new GalleryItemAdapter(mItems));
         }else{
             mGridView.setAdapter(null);
         }
@@ -60,6 +91,24 @@ public class PhotoGalleryFragment extends Fragment {
         protected void onPostExecute(ArrayList<GalleryItem> items){
             mItems = items;
             setupAdapter();
+        }
+    }
+
+    private class GalleryItemAdapter extends ArrayAdapter<GalleryItem>{
+        public GalleryItemAdapter(ArrayList<GalleryItem> items){
+            super(getActivity(),0,items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            if(convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.gallery_item,parent,false);
+            }
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.gallery_item_imageView);
+            imageView.setImageResource(R.mipmap.brian_up_close);
+            GalleryItem item = getItem(position);
+            mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+            return convertView;
         }
     }
 
